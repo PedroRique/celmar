e10a5995babdb2c96925271b05658b0488724920
Pedro Henrique de Oliveira Rique - PRI <pedro.rique@iteris.com.br>
2022-04-01 16:31:08 -0300

    atualizando projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true
 };
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDWtUwRO6mUZ4sj9qIVLhhm5XtF3bv9Tts",
-  authDomain: "celmarrio.firebaseapp.com",
-  databaseURL: "https://celmarrio-default-rtdb.firebaseio.com",
-  projectId: "celmarrio",
-  storageBucket: "celmarrio.appspot.com",
-  messagingSenderId: "836646201453",
-  appId: "1:836646201453:web:71833571fc0f0ebaebf764"
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false
-};
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDWtUwRO6mUZ4sj9qIVLhhm5XtF3bv9Tts",
-  authDomain: "celmarrio.firebaseapp.com",
-  databaseURL: "https://celmarrio-default-rtdb.firebaseio.com",
-  projectId: "celmarrio",
-  storageBucket: "celmarrio.appspot.com",
-  messagingSenderId: "836646201453",
-  appId: "1:836646201453:web:71833571fc0f0ebaebf764"
 };
 
 /*
@@ -23,4 +13,4 @@ export const firebaseConfig = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
